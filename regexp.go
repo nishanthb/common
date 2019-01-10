d301961c51fa12e2343ed6b31ccae76c9fffbbe7
Nishanth B <nishanthb@users.noreply.github.com>
2019-01-10 22:11:58 +0530

    Update regexp.go

--- regexp.go
+++ regexp.go
@@ -3,6 +3,7 @@ package common
 import "regex"
 
 // if r.MatchString(s) {...}
+// for named regexps - regexp.MustCompile(`(?P<name1>\d+)(?P<name2>\D+)...
 func SplitRegexMap(r *regexp, s string) map[string]string {
 	match := r.FindStringSubmatch(s)
 	results := map[string]string{}
